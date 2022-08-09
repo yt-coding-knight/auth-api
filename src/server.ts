1c46654242fc374e151c99784aa4c0531c451f45
yt-coding-knight <muh.sholeh.dev@gmail.com>
2022-08-09 10:15:16 +0900

    adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
-import config from "./app/config";
+import config, { corsOptions } from "./app/config";
 import authRouter from "./app/auth/router";
 import commentRouter from "./app/comment/router";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const port = config.port || 3000;
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
