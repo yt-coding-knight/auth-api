e093a53e0ef1df14d176ba060cf858f012eb8730
yt-coding-knight <muh.sholeh.dev@gmail.com>
2022-08-15 10:54:32 +0900

    prepare setting cors

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,21 +1,7 @@
-import { CorsOptions } from "cors";
-
 const config = {
   port: process.env.PORT,
   accessKey: process.env.ACCESS_KEY,
   refreshKey: process.env.REFRESH_KEY,
-};
-
-const whitelist = ["http://yourdomain.com", "http://localhost:5000"];
-export const corsOptions: CorsOptions = {
-  origin(origin, cb) {
-    if (whitelist.indexOf(origin as string) !== -1 || !origin) {
-      cb(null, true);
-    } else {
-      cb(new Error("Not allowed by cors"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
 import express from "express";
-import config, { corsOptions } from "./app/config";
+import config from "./app/config";
 import authRouter from "./app/auth/router";
 import commentRouter from "./app/comment/router";
 import cookieParser from "cookie-parser";
 import notFound from "./middleware/404";
 import errorHandle from "./middleware/errorHandler";
-import cors from "cors";
 
 const app = express();
 const port = config.port || 3000;
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
